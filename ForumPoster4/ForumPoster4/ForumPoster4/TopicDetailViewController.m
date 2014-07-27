973c62e632b3c1d4db759610113ec16ce8263a8d
mac <mac@BMdeiMac.local>
2014-07-27 16:55:05 +0800

    add a scrollView to show all of the content. can resize the height automatically.

--- ForumPoster4/ForumPoster4/ForumPoster4/TopicDetailViewController.m
+++ ForumPoster4/ForumPoster4/ForumPoster4/TopicDetailViewController.m
@@ -10,9 +10,12 @@
 #import "Images.h"
 
 
+
 @interface TopicDetailViewController () <UIScrollViewDelegate>
-@property (weak, nonatomic) IBOutlet UILabel *labelTitle;
-@property (weak, nonatomic) IBOutlet UITextView *textViewContent;
+
+@property (strong, nonatomic)UIScrollView *scrollViewContent;
+//@property (strong, nonatomic) UILabel *labelTitle;
+//@property (strong, nonatomic) UITextView *textViewContent;
 @property (strong, nonatomic) UIScrollView *imagesScrollView;
 
 
@@ -33,19 +36,60 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.navigationController.title = @"帖子详情";
+    self.navigationItem.title = @"帖子详情";
     
-    self.labelTitle.text = self.topic.title;
-    self.textViewContent.text = self.topic.content;
+    CGRect titleRect = CGRectMake(10, 70, self.view.bounds.size.width - 20, 30);
+    CGRect contentRect = CGRectZero;
+    
+    UILabel *labelTitle = [[UILabel alloc]initWithFrame:titleRect];
+    UITextView *textViewContent = [[UITextView alloc]initWithFrame:contentRect];
+    
+    labelTitle.text = self.topic.title;
+    textViewContent.text = self.topic.content;
     
     
     
+    self.scrollViewContent = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+    _scrollViewContent.delegate = self;
+    //_scrollViewContent.backgroundColor = [UIColor lightGrayColor];
+    
+    
+    
+    labelTitle.frame = titleRect;
+    textViewContent.frame = contentRect;
+    
+    [_scrollViewContent addSubview:labelTitle];
+    [_scrollViewContent addSubview:textViewContent];
+    
+    
+
+    
+    [self.view addSubview:_scrollViewContent];
+    
+    textViewContent.frame = CGRectMake(10, 120, self.view.bounds.size.width - 20, 500);
+    CGFloat fixedWidth = self.view.bounds.size.width - 20.0;
+    CGSize newSize = [textViewContent sizeThatFits:CGSizeMake(fixedWidth, MAXFLOAT)];
+    CGRect newFrame = textViewContent.frame;
+    newFrame.size = CGSizeMake(fixedWidth, newSize.height);
+    textViewContent.frame = newFrame;
+    
+    _scrollViewContent.contentSize = CGSizeMake(self.view.bounds.size.width, 100 + textViewContent.frame.size.height);
+    
+    
+    
+    
+    // Config images
     if (self.topic.images.count > 0) {
-        NSArray *imagesEntityArray = [self.topic.images allObjects];
-        NSLog(@"There are %lu pictures in this topic.", (unsigned long)imagesEntityArray.count);
+        //move content view down 170
         
-        // Config images
-        self.imagesScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(10, 300, 300, 170)];
+        CGRect newFrame = textViewContent.frame;
+        newFrame.origin = CGPointMake(10, 290);
+        textViewContent.frame = newFrame;
+        
+        
+        NSArray *imagesEntityArray = [self.topic.images allObjects];
+        //NSLog(@"There are %lu pictures in this topic.", (unsigned long)imagesEntityArray.count);
+        self.imagesScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(10, 120, 300, 170)];
         self.imagesScrollView.delegate = self;
         self.imagesScrollView.contentSize = CGSizeMake(320, 170);
         self.imagesScrollView.backgroundColor = [UIColor lightGrayColor];
@@ -62,10 +106,12 @@
             imageView.layer.masksToBounds = YES;
             [self.imagesScrollView addSubview:imageView];
         }
-        
+        [self.scrollViewContent addSubview:_imagesScrollView];
     }
     
+    NSLog(@"content height:%f\n", textViewContent.frame.size.height);
 }
+
 
 
 
